19c75ec58b8b0c6058ee8ad4f96d504219fdad71
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-08-08 23:03:19 +0200

    releaser
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# `ttrpcurl` - Confidential Containers Edition
+
+This repo releases binaries of [`ttrpcurl`](https://github.com/katexochen/ttrpcurl) with embedded proto sources of the [Confidential Containers project](https://github.com/confidential-containers), of which it isn't an official part.
+
+All proto files versioned in this repository as well as the ones embedded in the distributed binaries are licensed under [Apache 2.0 License](https://github.com/confidential-containers/confidential-containers/blob/main/LICENSE) and `Copyright (c) The Confidential Containers Contributors`.
--- pull-protos.sh
+++ pull-protos.sh
@@ -17,29 +17,46 @@ function downloadRepoArchive() {
 
 function main() {
     repos=$(ghAPI "/orgs/${org}/repos?type=source&per_page=100")
-    repos=$(jq --raw-output '.[] | select( .archived == false and .size != 0 ) | .name' <<< "$repos")
+    repoNames=$(
+        jq --raw-output '.[] | select(
+            .archived == false and
+            .size != 0 and
+            .license.spdx_id == "Apache-2.0"
+        ) | .name' <<< "$repos"
+    )
 
     targetPath="$(pwd)/protos"
     pushd "$(mktemp -d)" > /dev/null
 
-    for repo in $repos; do
+    for repo in $repoNames; do
         echo "Downloading protos from ${repo}"
 
         downloadRepoArchive "$repo"
         mkdir "${repo}"
-        tar -xzf "${repo}.tar.gz" -C "${repo}" --strip-components=1
+        tar -xzf "${repo}.tar.gz"
         rm "${repo}.tar.gz"
-        pushd "${repo}" > /dev/null
+        unpackDir=$(ls -d "${org}-${repo}"*)
+        ref=${unpackDir##*-}
+        pushd "${unpackDir}" > /dev/null
 
         protos=$(find . -type f -name '*.proto' -not -path '*/vendor/*')
 
+        copied=0
         for proto in $protos; do
             echo "  ${proto}"
 
             filename=$(basename "${proto}")
+            filenamePrefix=${filename%%.proto}
+
+            if ! grep -q "package ${filenamePrefix};" "${proto}"; then
+                # Fix package name to the filename to prevent collisions
+                sed -i "s/^package .*;$/package ${filenamePrefix};/" "${proto}"
+                continue
+            fi
 
             if [[ ! -e "${targetPath}/${filename}" ]]; then
                 cp -n "${proto}" "${targetPath}/${filename}"
+                copied=$((copied + 1))
                 continue
             fi
 
@@ -50,6 +67,10 @@ function main() {
             diff "$proto" "${targetPath}/${filename}" || true
             echo
         done
+
+        if [[ $copied -ne 0 ]]; then
+            echo "${org}/${repo} ${ref}" >> "${targetPath}/.refs"
+        fi
 
         popd > /dev/null
         rm -rf "${repo}"
